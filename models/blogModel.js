1482433596b34202e08115a935bf9d5f3158bf46
Ivan Maksimov <ivan_kratovo@hotmail.com>
2023-02-15 13:26:49 +0100

    blog like dislike added

--- models/blogModel.js
+++ models/blogModel.js
@@ -1,1 +1,64 @@
-const
+const mongoose = require("mongoose"); // Erase if already required
+
+// Declare the Schema of the Mongo model
+var blogSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    numViews: {
+      type: Number,
+      default: 0,
+    },
+    isLiked: {
+      type: Boolean,
+      default: false,
+    },
+    isDisliked: {
+      type: Boolean,
+      default: false,
+    },
+    likes: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    dislikes: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    image: {
+      type: String,
+      default:
+        "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.istockphoto.com%2Fphotos%2Fblog&psig=AOvVaw192St6zdqGYC6qJYVtg9Gn&ust=1676468515021000&source=images&cd=vfe&ved=0CA8QjRxqFwoTCKCR8oOSlf0CFQAAAAAdAAAAABAE",
+    },
+    author: {
+      type: String,
+      default: "Admin",
+    },
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    toObject: {
+      virtuals: true,
+    },
+    timestamps: true,
+  }
+);
+
+//Export the model
+module.exports = mongoose.model("Blog", blogSchema);
