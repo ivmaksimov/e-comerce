ef946d78dafc98524d5673589b8e9953611e4b25
Ivan Maksimov <ivan_kratovo@hotmail.com>
2023-02-16 10:10:37 +0100

    updated prodcat added

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const PORT = process.env.PORT || 4000;
 const authRouter = require("./routes/authRoute");
 const productRouter = require("./routes/productRoute");
 const blogRouter = require("./routes/blogRoute");
+const categoryRouter = require("./routes/prodCategoryRoute");
+const blogcategoryRouter = require("./routes/blogCatRoute");
 const cookieParser = require("cookie-parser");
 const morgan = require("morgan");
 
@@ -22,6 +24,8 @@ app.use(cookieParser());
 app.use("/api/user", authRouter);
 app.use("/api/product", productRouter);
 app.use("/api/blog", blogRouter);
+app.use("/api/category", categoryRouter);
+app.use("/api/blogcategory", blogcategoryRouter);
 
 app.use(notFound);
 app.use(errorHandler);
